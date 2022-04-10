1aceaad692d7f98de3338597da706d8c51f7e1a0
valerieJJ <820439600@qq.com>
2022-04-11 00:40:20 +0800

    order-service mybatis generator

--- order-service/src/main/java/vjj/orderservice/config/ApplicationContextConfig.java
+++ order-service/src/main/java/vjj/orderservice/config/ApplicationContextConfig.java
@@ -1,11 +1,13 @@
 package vjj.orderservice.config;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
+@MapperScan("vjj.orderservice.mapper")
 public class ApplicationContextConfig {
     @Bean
     @LoadBalanced
