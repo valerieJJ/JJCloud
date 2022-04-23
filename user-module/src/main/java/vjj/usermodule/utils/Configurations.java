0bd948d22095a5ccbec3ae3834a76d1e928284e2
valerieJJ <820439600@qq.com>
2022-04-24 02:19:53 +0800

    multiple spring profiles

--- user-module/src/main/java/vjj/usermodule/utils/Configurations.java
+++ user-module/src/main/java/vjj/usermodule/utils/Configurations.java
@@ -1,35 +1,15 @@
 package vjj.usermodule.utils;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.core.io.Resource;
 import redis.clients.jedis.Jedis;
-
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.net.UnknownHostException;
-import java.util.Properties;
 
 @Configuration
 public class Configurations {
 
-    private final String mongoHost = "localhost";
-    private final int mongoPort = 27017;
-    private final String esClusterName = "jj-cluster";
-    private final String esHost = "localhost";
-    private final int esPort =  9300;
-    private final String redisHost;
-
-
-    public Configurations() throws IOException {
-        Properties properties = new Properties();
-        Resource resource = new ClassPathResource("application.properties");
-//        properties.load(new FileInputStream(resource.getFile()));
-        properties.load(resource.getInputStream());
-        this.redisHost = properties.getProperty("spring.redis.host");
-    }
-
+    @Value("${spring.redis.host}")
+    private String redisHost;
 
     @Bean(name = "jedis")
     public Jedis getRedisClient(){
